347beb84e73c67f87e1a759394eb92e089e8622c
Giftlin DK <80969422+Giftlin27@users.noreply.github.com>
2023-12-31 17:38:34 +0530

    Update.java

--- Arrays(Easy).java
+++ Arrays(Easy).java
@@ -208,6 +208,28 @@ class Solution {
 12. [Find the Highest Altitude](https://leetcode.com/problems/find-the-highest-altitude/)
 class Solution {
     public int largestAltitude(int[] gain) {
+        int[] arr = new int[gain.length+1];
+        arr[0]=0;
+        for(int i=0; i<gain.length; i++)
+        {
+            arr[i+1]=arr[i]+gain[i];
+        }
+        int max = arr[0];
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+}
+
+                                
+ //better                               
+class Solution {
+    public int largestAltitude(int[] gain) {
         int max=0;
         int cal=0;
         for(int i=0; i<gain.length; i++)
